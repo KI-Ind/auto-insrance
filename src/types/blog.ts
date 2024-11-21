c93f24724db84f69f7d31850e3f74ea88fb340e1
KI <160818528+KI-Ind@users.noreply.github.com>
2024-11-22 00:20:51 +0500

    Create blog.ts

---
+++ src/types/blog.ts
@@ -0,0 +1,15 @@
+export interface BlogArticle {
+  id: string;
+  title: string;
+  description: string;
+  date: string;
+  author: string;
+  image: string;
+  category: string;
+  readTime: string;
+  content: string;
+  tags: string[];
+  metaTitle: string;
+  metaDescription: string;
+  slug: string;
+}
